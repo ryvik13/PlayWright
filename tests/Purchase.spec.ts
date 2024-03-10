adfe09d16dbb68776a764264e856c78b8763d0b9
Evgeny Tolchinsky <evgeny.tol@gmail.com>
2024-03-10 18:15:20 +0200

    Update Purchase.spec.ts

--- tests/Purchase.spec.ts
+++ tests/Purchase.spec.ts
@@ -4,6 +4,7 @@ test('Purchase the order', async ({ page }) => {
 const email = 'evgeny.tol@gmail.com'
 const pass = '123456'
 await page.goto('https://shopdemo-alex-hot.koyeb.app/');
+//login
 await page.getByRole('link', { name: 'Welcome! ' }).click();
 await page.getByRole('menuitem', { name: 'Login' }).click();
 await page.getByRole('main').getByPlaceholder('Please Enter Your Email').click();
@@ -11,8 +12,12 @@ await page.getByRole('main').getByPlaceholder('Please Enter Your Email').fill(em
 await page.getByPlaceholder('Please Enter Your Password').click();
 await page.getByPlaceholder('Please Enter Your Password').fill(pass);
 await page.getByRole('button', { name: 'Login' }).click();
+//purchase
 await page.getByRole('link', { name: 'Shop' }).click();
 await page.getByRole('link', { name: 'CHERRY TOMATOES By Nizhyn' }).click();
+await page.getByRole('button', { name: 'Add To Bag' }).click();
+await page.getByRole('button', { name: 'Continue Shopping' }).click();
+await page.getByRole('link', { name: 'MARINATED CUCUMBERS NEZHIN STYLE' }).click();
 await page.getByRole('button', { name: 'Add To Bag' }).click();
 await page.getByRole('button', { name: 'Place Order' }).click();
 await expect(page.getByRole('heading', {name:'Thank you for your order.'})).toBeVisible();
